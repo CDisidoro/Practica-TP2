c1f67c9854459a291af780efc9d3689104a6227e
jose ignacio barrios oros <49521251+JoseIgnacioBarrios@users.noreply.github.com>
2021-03-04 12:52:18 +0100

    mod Factory

--- PhysicsSimulator/src/simulator/factories/Factory.java
+++ PhysicsSimulator/src/simulator/factories/Factory.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.json.JSONObject;
 
 public interface Factory<T> {
-	public T createInstance(JSONObject info);
+	public T createInstance(JSONObject info) throws IllegalArgumentException;
 
 	public List<JSONObject> getInfo();
 }
