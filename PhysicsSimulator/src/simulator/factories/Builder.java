1363cd75215089c56326b27839ec7be9c7c695d1
jose ignacio barrios oros <49521251+JoseIgnacioBarrios@users.noreply.github.com>
2021-03-05 18:48:06 +0100

    mod Builder

--- PhysicsSimulator/src/simulator/factories/Builder.java
+++ PhysicsSimulator/src/simulator/factories/Builder.java
@@ -4,40 +4,24 @@ import org.json.JSONObject;
 
 public abstract class Builder<T> {
 	protected String typeTag, desc;
-	public Builder(String typeTag, String desc) {
-		this.typeTag = typeTag;
-		this.desc = desc;
-	}
-	
+		
 	public T createInstance(JSONObject info) throws IllegalArgumentException {
-		//Pendiente de Programar
-		/*typeTag = info.getString("type");
-		switch(typeTag) {
-		case "basic":
-			
-			break;
-		case "mlb":
-			
-			break;
-		default:
-			return null;
-		}*/
 		 T b = null;
 		 if (typeTag != null && typeTag.equals(info.getString("type")))
-			 return null;//b = ? crear un objeto de tipo 
+			 b= createTheInstance(info.getJSONObject("data"));
 		 return b;
 	}
 	
 	public JSONObject getBuilderInfo() {
 		JSONObject info = new JSONObject();
 		 info.put("type", typeTag);
-		 info.put("data", new JSONObject());
+		 info.put("data", createData());
 		 info.put("desc", desc);
 		 return info;
 	}
 	
 	protected JSONObject createData() {
-		//Pendiente de Programar
+		return new JSONObject();
 	}
 	
 	protected abstract T createTheInstance(JSONObject info);
