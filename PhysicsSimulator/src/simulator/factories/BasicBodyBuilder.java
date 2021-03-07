e651a600205c3a2609766a56b3fef2fa41126205
jose ignacio barrios oros <49521251+JoseIgnacioBarrios@users.noreply.github.com>
2021-03-07 21:32:49 +0100

    modificacion a verificar tutoria

--- PhysicsSimulator/src/simulator/factories/BasicBodyBuilder.java
+++ PhysicsSimulator/src/simulator/factories/BasicBodyBuilder.java
@@ -6,17 +6,29 @@ import simulator.model.Body;
 public class BasicBodyBuilder extends Builder<Body>{
 
 	public BasicBodyBuilder() {
-		//Pendiente de Programar
+		this.typeTag="basic";
+		this.desc="cuerpoBasico";
 	}
 	
 	@Override
-	protected Body createTheInstance(JSONObject info) {
-		//Pendiente de Programar
-		return null;
+	protected Body createTheInstance(JSONObject data) {
+		String id= data.getString("id");
+		Vector2D vel= ;//como sacar el vector de data tipo JSONObject
+		Vector2D pos = ;
+		double mass = data.getDouble("mass");
+		
+		return new Body(id,vel,pos,mass);
 	}
 	
-	protected JSONObject createData() {
-		//Pendiente de Programar
+	public JSONObject createData() {
+		JSONObject data = new JSONObject();
+
+		data.put("id", //????);///uqe pongo aqui?
+		data.put("pos", //???);
+		data.put("vel", );
+		data.put("mass", );
+		
+		return data;
 	}
 	
 }
