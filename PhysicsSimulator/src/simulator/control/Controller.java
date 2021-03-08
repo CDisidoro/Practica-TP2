5a8032bd45ede3220ddcd6e1c55195099e93c625
Camilo Andres D'isidoro <camilodisidoro2015@gmail.com>
2021-03-08 16:04:16 +0100

    Implementacion LocalBodies

--- PhysicsSimulator/src/simulator/control/Controller.java
+++ PhysicsSimulator/src/simulator/control/Controller.java
@@ -2,6 +2,11 @@ package simulator.control;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import simulator.factories.Factory;
 import simulator.model.Body;
 import simulator.model.PhysicsSimulator;
@@ -20,6 +25,10 @@ public class Controller {
 	}
 	
 	public void localBodies(InputStream input) {
-		// Pendiente de Programar
+		JSONObject jinput = new JSONObject(new JSONTokener(input));
+		JSONArray array = jinput.getJSONArray("bodies");
+		for(int i = 0; i < array.length(); i++) { //Mirar si se puede mejorar a un for-each
+			simulador.addBody(factoria.createInstance(array.getJSONObject(i)));
+		}
 	}
 }
