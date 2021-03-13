ccffd637fa56898c95df510de75487b4b38f8ce0
Camilo Andres D'isidoro <camilodisidoro2015@gmail.com>
2021-03-13 15:42:25 +0100

    Documentacion y buildOptions de Main
    - Se han agregado las opciones pendientes del Main
    - Se han documentado todos los métodos de Main
    To-Do:
    - Implementar el startBatchMode
    - Implementar el parseArgs
    - Implementar el init

--- PhysicsSimulator/src/simulator/launcher/Main.java
+++ PhysicsSimulator/src/simulator/launcher/Main.java
@@ -16,24 +16,24 @@ import simulator.model.ForceLaws;
 
 public class Main {
 
-	// default values for some parameters
-	//
+	// Valores por defecto para algunos parametros
 	private final static Double _dtimeDefaultValue = 2500.0;
 	private final static String _forceLawsDefaultValue = "nlug";
 	private final static String _stateComparatorDefaultValue = "epseq";
+	private final static int _stepsDefaultValue = 150;
 
-	// some attributes to stores values corresponding to command-line parameters
-	//
+	// Algunos atributos para almacenar valores correspondientes a los parametros de la linea de comandos
 	private static Double _dtime = null;
 	private static String _inFile = null;
 	private static JSONObject _forceLawsInfo = null;
 	private static JSONObject _stateComparatorInfo = null;
 
-	// factories
+	// Factorias
 	private static Factory<Body> _bodyFactory;
 	private static Factory<ForceLaws> _forceLawsFactory;
 	private static Factory<StateComparator> _stateComparatorFactory;
 
+	//Inicializador de los componentes de la simulacion (Comparadores de Estado, Factorias de Leyes y Cuerpos) - TODO
 	private static void init() {
 		// TODO initialize the bodies factory
 
@@ -42,6 +42,7 @@ public class Main {
 		// TODO initialize the state comparator
 	}
 
+	//Parseo de los argumentos de la linea de comandos - TODO
 	private static void parseArgs(String[] args) {
 
 		// define the valid command line options
@@ -80,32 +81,43 @@ public class Main {
 
 	}
 
+	//Construye el Menu de Opciones del Simulador
 	private static Options buildOptions() {
 		Options cmdLineOptions = new Options();
 
-		// help
+		// Ayuda
 		cmdLineOptions.addOption(Option.builder("h").longOpt("help").desc("Print this message.").build());
 
-		// input file
+		// Archivo de Entrada
 		cmdLineOptions.addOption(Option.builder("i").longOpt("input").hasArg().desc("Bodies JSON input file.").build());
-
-		// TODO add support for -o, -eo, and -s (add corresponding information to
-		// cmdLineOptions)
-
-		// delta-time
+		
+		// Salida Esperada
+		cmdLineOptions.addOption(Option.builder("eo").longOpt("expected-output").hasArg()
+				.desc("The expected output file. If not provided no comparison is applied")
+				.build());
+		
+		// Archivo de Salida
+		cmdLineOptions.addOption(Option.builder("o").longOpt("output").hasArg()
+				.desc("Output file, where output is written. Default value: the standard output").build());
+		
+		// Pasos
+		cmdLineOptions.addOption(Option.builder("s").longOpt("steps").hasArg()
+				.desc("An integer = number of steps. Default: " + _stepsDefaultValue).build());
+		
+		// Tiempo Delta
 		cmdLineOptions.addOption(Option.builder("dt").longOpt("delta-time").hasArg()
 				.desc("A double representing actual time, in seconds, per simulation step. Default value: "
 						+ _dtimeDefaultValue + ".")
 				.build());
 
-		// force laws
+		// Leyes de Fuerza
 		cmdLineOptions.addOption(Option.builder("fl").longOpt("force-laws").hasArg()
 				.desc("Force laws to be used in the simulator. Possible values: "
 						+ factoryPossibleValues(_forceLawsFactory) + ". Default value: '" + _forceLawsDefaultValue
 						+ "'.")
 				.build());
 
-		// gravity laws
+		// Comparador de Leyes de Gravedad
 		cmdLineOptions.addOption(Option.builder("cmp").longOpt("comparator").hasArg()
 				.desc("State comparator to be used when comparing states. Possible values: "
 						+ factoryPossibleValues(_stateComparatorFactory) + ". Default value: '"
@@ -114,13 +126,15 @@ public class Main {
 
 		return cmdLineOptions;
 	}
-
+	
+	//Detecta los posibles valores para una Factoria
 	public static String factoryPossibleValues(Factory<?> factory) {
+		//Si la factoria es nula, retorna un mensaje diciendo que No hay posibles valores para la Factoria
 		if (factory == null)
 			return "No values found (the factory is null)";
 
 		String s = "";
-
+		//Para cada JSONObject dentro del getInfo de factory, se concatenara el tipo y descripcion de dato posible de la factoria
 		for (JSONObject fe : factory.getInfo()) {
 			if (s.length() > 0) {
 				s = s + ", ";
@@ -132,6 +146,7 @@ public class Main {
 		return s;
 	}
 
+	//Parseo de la Opcion de Ayuda
 	private static void parseHelpOption(CommandLine line, Options cmdLineOptions) {
 		if (line.hasOption("h")) {
 			HelpFormatter formatter = new HelpFormatter();
@@ -140,6 +155,7 @@ public class Main {
 		}
 	}
 
+	//Parseo de la Opcion de Archivo de Entrada que lanza un ParseException si no se especifica un archivo de entrada para el modo batch
 	private static void parseInFileOption(CommandLine line) throws ParseException {
 		_inFile = line.getOptionValue("i");
 		if (_inFile == null) {
@@ -147,6 +163,7 @@ public class Main {
 		}
 	}
 
+	//Parseo de la Opcion del Tiempo Delta que lanza un ParseException si la informacion del tiempo delta es invalida
 	private static void parseDeltaTimeOption(CommandLine line) throws ParseException {
 		String dt = line.getOptionValue("dt", _dtimeDefaultValue.toString());
 		try {
@@ -157,6 +174,7 @@ public class Main {
 		}
 	}
 
+	//Parsea el tipo de objeto recibido en el String con el formato (tipo:dato) o (tipo) en un JSONObject
 	private static JSONObject parseWRTFactory(String v, Factory<?> factory) {
 
 		// the value of v is either a tag for the type, or a tag:data where data is a
@@ -164,18 +182,20 @@ public class Main {
 		// information
 		// into variables 'type' and 'data'
 		//
+		//Busca el separador entre el tipo y el dato en el string v
 		int i = v.indexOf(":");
 		String type = null;
 		String data = null;
+		//Si se ha encontrado el separador en el string, creara dos substring type y data separados por la posicion del separador
 		if (i != -1) {
 			type = v.substring(0, i);
 			data = v.substring(i + 1);
-		} else {
+		} else { //En caso opuesto el tipo sera igual al string v
 			type = v;
 			data = "{}";
 		}
 
-		// look if the type is supported by the factory
+		// Comprueba si el tipo es soportado por la Factoria
 		boolean found = false;
 		for (JSONObject fe : factory.getInfo()) {
 			if (type.equals(fe.getString("type"))) {
@@ -184,7 +204,7 @@ public class Main {
 			}
 		}
 
-		// build a corresponding JSON for that data, if found
+		// Si se encuentra una Factoria para el objeto, creara un JSONObject para este
 		JSONObject jo = null;
 		if (found) {
 			jo = new JSONObject();
@@ -195,6 +215,7 @@ public class Main {
 
 	}
 
+	//Parseo de la Opcion de Ley de Fuerza, que lanza un ParseException si la informacion de la ley es invalida
 	private static void parseForceLawsOption(CommandLine line) throws ParseException {
 		String fl = line.getOptionValue("fl", _forceLawsDefaultValue);
 		_forceLawsInfo = parseWRTFactory(fl, _forceLawsFactory);
@@ -203,6 +224,7 @@ public class Main {
 		}
 	}
 
+	//Parseo de la Opcion de Comparador de Estados que lanza un ParseException si la informacion del comparador es invalida
 	private static void parseStateComparatorOption(CommandLine line) throws ParseException {
 		String scmp = line.getOptionValue("cmp", _stateComparatorDefaultValue);
 		_stateComparatorInfo = parseWRTFactory(scmp, _stateComparatorFactory);
@@ -211,15 +233,18 @@ public class Main {
 		}
 	}
 
+	//Inicio del modo Batch - TODO
 	private static void startBatchMode() throws Exception {
 		// TODO complete this method
 	}
 
+	//Funcion de inicio del simulador que llama al parseo de los argumentos pasados e inicia el modo Batch
 	private static void start(String[] args) throws Exception {
 		parseArgs(args);
 		startBatchMode();
 	}
 
+	//Funcion principal de ejecución que llama a init y a start
 	public static void main(String[] args) {
 		try {
 			init();
